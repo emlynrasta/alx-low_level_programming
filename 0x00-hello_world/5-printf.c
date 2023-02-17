e63e3d4ae84770fa5ff238b3a71f890345ae3619
emlynrasta <rastablackman21@gmail.com>
2023-02-17 00:10:08 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints using printf
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char mystr[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s", mystr);
+
+	return (0);
+}
