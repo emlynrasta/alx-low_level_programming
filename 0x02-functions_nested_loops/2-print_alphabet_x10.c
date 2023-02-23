23e00e4c9c782104994d65959804b56b491b73b2
emlynrasta <rastablackman21@gmail.com>
2023-02-22 17:14:28 -0800

    6-abs.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,10 +10,10 @@ void print_alphabet_x10(void)
 {
 	char i, j;
 
-	for (i = 0; i < 11; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (j = 'a'; j <= 'z'; j++)
-			putchar(i);
-	printf('\n');
+			_putchar(i);
+	_putchar('\n');
 	}
 }
--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main -
- * Description -
- * @c -
+ * _abs - computes the absolute value of an integer
+ * @c : int to be computed
  *
  * Return: always 0
  */
 
-int main(void)
+int _abs(int c)
 {
+	if (c < 0)
+	{
+		int abs_val;
+
+		ads_val = c * -1;
+
+		return (abs_val);
 
 	return (0);
 }
