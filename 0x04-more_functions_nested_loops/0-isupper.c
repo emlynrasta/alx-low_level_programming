3e71b4ad121ca70e2bc2442dac873a8befbe7d5d
emlynrasta <rastablackman21@gmail.com>
2023-02-23 20:19:04 +0300

    0-isupper - tell if character is uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+
+#include "main.h"
+/**
+ * _isupper - checks if a character is uppercase
+ * @c : chracter to be checked
+ * Return: 1 for uppercase , 0 otherwise
+ */
+
+int _isupper(int c)
+{
+    if (c >= 65 && c <= 90)
+    return (1);
+
+	return (0);
+}
