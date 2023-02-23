277cf106efa063b9c7f9f77283fc716852632453
emlynrasta <rastablackman21@gmail.com>
2023-02-23 13:42:13 -0800

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,16 +7,15 @@
 
 void print_line(int n)
 {
-    int i;
+	int i;
 
-    if (n <= 0)
-    {
-        _putchar('\n');
-    } else
-    {
-        for (i = 0; i <= n; i++)
-        {
-            _putchar('_');
-        }
-    }
+	if (n > 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+	_puchar('\n');
 }
