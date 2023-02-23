0d5a97099d36e417b42848c691807fce8c54a775
emlynrasta <rastablackman21@gmail.com>
2023-02-22 16:36:19 -0800

    5-sign.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main -
+ * Description -
+ * @c -
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
-int putchar(int a);
+int _putchar(int a);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
