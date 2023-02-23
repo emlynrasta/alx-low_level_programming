30e7d16c1836799868f6018d8bfc3d9707ac3734
emlynrasta <rastablackman21@gmail.com>
2023-02-22 16:18:20 -0800

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_sign - prints the sign of a number
+ * Description - 
+ * @c -
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+
+	return (0);
+}
