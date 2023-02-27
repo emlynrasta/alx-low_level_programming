294dc8da2bcee2fbf4242cd998e72c6182caa17a
emlynrasta <rastablackman21@gmail.com>
2023-02-27 01:43:14 -0800

    reversed that string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s : string to be reversed
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0';)
+		i++;
+
+	for (; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
