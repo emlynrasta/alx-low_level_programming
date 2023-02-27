c382a44c60d7f048f0e859e841ca843b22e6d57d
emlynrasta <rastablackman21@gmail.com>
2023-02-27 02:22:01 -0800

    reveresed that string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s : string to be reversed
+ *
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int i;
+	char rev[10];
+	int j;
+
+	for (i = 0; s[i] != '\0';)
+		i++;
+
+	for (; i >= 0; i--)
+	{
+		if (s[i] != '\0')
+		{
+			for (j = 0; j++;)
+				rev[j] = s[i];
+		}
+	}
+	s = rev;
+}
