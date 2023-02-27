e33cf201f804c9bbd691284fcac34f6b4ed128a3
emlynrasta <rastablackman21@gmail.com>
2023-02-27 07:47:34 -0800

    printing second half of string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * puts_half - prints second half of a string
+ * @str : string to be split
+ *
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int i, j, mid;
+
+	j = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+		j++;
+
+	if (j % 2 == 0)
+	{
+		mid = j / 2;
+	} else if (j % 2 != 0)
+	{
+		mid = (j - 1) / 2;
+	}
+
+	for (; str[mid] != '\0'; mid++)
+		_putchar(str[mid]);
+	_putchar('\n');
+}
